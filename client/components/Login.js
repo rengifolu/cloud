c1ccab5e50265d1a4c8c77567415db915dec2a13
diego armando <diego@mbp-de-andre.speedport.ip>
2020-03-23 20:54:31 +0100

    post axios componente login

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,16 +1,80 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 import '../css/Login.css';
+import axios from 'axios';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleTextChange(e) {
+    if (e.target.name === "email") {
+      this.setState({
+        email: e.target.value
+      });
+    }
+    if (e.target.name === "password") {
+      this.setState({
+        password: e.target.value
+      });
+    }
+  }
+  onClick(e) {
+    this.handleClick(this);
+  }
+  handleClick(event) {
+    var self = this;
+    var payload = {
+      "email": this.state.email,
+      "password": this.state.password
+    }
+    axios.post('./login', payload)
+      .then(function (response) {
+        console.log(response);
+        if (response.data.code == 200) {
+          console.log("Login successfull");
+          var uploadScreen = [];
+          uploadScreen.push(<UploadScreen appContext={self.props.appContext} />)
+          self.props.appContext.setState({ loginPage: [], uploadScreen: uploadScreen })
+        }
+        else if (response.data.code == 204) {
+          console.log("Username password do not match");
+          alert("username password do not match")
+        }
+        else {
+          console.log("Username does not exists");
+          alert("Username does not exist");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  handleSubmit(event) {
+    console.log('login ');
+    //ccalert('A name was submitted: ');
+    event.preventDefault();
+  }
 
   render() {
     return (
       <div className="Login">
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={this.state.email}
+              onChange={this.handleTextChange}
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
           </Form.Text>
@@ -18,9 +82,18 @@ class Login extends React.Component {
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.handleTextChange}
+            />
           </Form.Group>
-          <Button variant="primary" type="submit">
+          <Button
+            variant="primary"
+            type="submit"
+            onClick={this.onClick}
+          >
             Submit
         </Button>
         </Form>
